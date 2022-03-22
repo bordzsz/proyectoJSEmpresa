5fe79738e6daceb950821b9cadf11d8ea771425e
Borja Diaz <borjad99@gmail.com>
2022-03-22 13:19:58 +0000

    Obtengo una frase random de una Categoria

--- chuck-borja-react/src/FraseDeCategoria.jsx
+++ chuck-borja-react/src/FraseDeCategoria.jsx
@@ -1,8 +1,24 @@
 import styles from "./FraseDeCategoria.module.css";
+import { useEffect, useState } from "react";
 
 export function FraseDeCategoria() {
-  
+  //https://api.chucknorris.io/jokes/random
+  //https://api.chucknorris.io/jokes/categories
+  //https://api.chucknorris.io/jokes/random?category=animal
+
+  const [categorias, setCategorias] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.chucknorris.io/jokes/random?category=animal")
+      .then((result) => result.json())
+      .then((data) => {
+        setCategorias(data);
+      });
+  }, []);
+
   return (
-    <h4 className={styles.fraseObtenida}>Hola buenas</h4>
+    <h4 className={styles.fraseObtenida}>
+      {categorias.value}
+    </h4>
   );
 }
