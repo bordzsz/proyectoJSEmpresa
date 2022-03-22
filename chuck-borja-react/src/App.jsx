c5c3c328620b0baa2511de5a3da68e85ac3e4de0
Borja Diaz <borjad99@gmail.com>
2022-03-22 11:19:59 +0000

    Empezando el Proyecto

---
+++ chuck-borja-react/src/App.jsx
@@ -0,0 +1,12 @@
+import { MostradoFrases } from "./MostradoFrases";
+
+export function App(){
+    return(<div>
+            <header>
+                <h1>Proyecto JS - Chuck Borja Díaz</h1>
+            </header>
+            <main>
+                <MostradoFrases />
+            </main>
+    </div>);
+}
---
+++ chuck-borja-react/src/MostradoFrases.jsx
@@ -0,0 +1,9 @@
+export function MostradoFrases() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  );
+}
